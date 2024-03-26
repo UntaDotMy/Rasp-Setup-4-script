f746ffb26241447866309d33c0624f938286c2c2
BilaUnta <42847931+UntaDotMy@users.noreply.github.com>
2024-03-26 13:53:01 +0800

    Update info2tele.sh - reformat

--- info2tele.sh
+++ info2tele.sh
@@ -15,24 +15,29 @@ function check_internet() {
 BOT_TOKEN="BOT_ID"
 CHAT_ID="CHAT_ID"
 
+# Function to send message to Telegram bot
+function send_telegram_message() {
+    local message="$1"
+    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d chat_id=$CHAT_ID -d text="$message"
+}
+
 # Wait for internet connection
-while ! check_internet
-do
+while ! check_internet; do
     echo "No internet connection on wlan0"
     sleep 10
 done
 
-#delay data for making sure the connection is going thru
+# Delay for ensuring the connection is stable
 sleep 5 
 
 # Get the IPv4 information
-IPV4_INFO=$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+IPV4_INFO=$(ip -4 addr show wlan0 | awk '/inet / {print $2}')
 
 # Get the SSID
 SSID=$(iwgetid -r)
 
 # Create the message
-MESSAGE="IPv4 Info: $IPV4_INFO, SSID: $SSID"
+MESSAGE="IPv4 Info: $IPV4_INFO\nSSID: $SSID"
 
 # Send the message to the Telegram bot
-curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d chat_id=$CHAT_ID -d text="$MESSAGE"
+send_telegram_message "$MESSAGE"
